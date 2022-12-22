d0e75bb775ddc5f517f8e7e3efde360e82f4ef12
jaombori <jovanni.auxilio@ombori.com>
2022-12-22 12:08:30 +0100

    first commit

---
+++ src/init.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useGridSignals } from '@ombori/grid-signals-react';
+import App from './app';
+
+
+const {
+  REACT_APP_DEVICE_ID,
+  REACT_APP_ACCESS_ID,
+  REACT_APP_ACCESS_TOKEN,
+  REACT_APP_SPACE_ID,
+  NODE_ENV
+} = process.env;
+
+const isProd = NODE_ENV === 'production';
+
+const Init = () => {
+  const isSignalsReady = useGridSignals(isProd ? {} : {
+    deviceId: REACT_APP_DEVICE_ID,
+    accessId: REACT_APP_ACCESS_ID,
+    accessToken: REACT_APP_ACCESS_TOKEN,
+    spaceId: REACT_APP_SPACE_ID,
+  });
+
+  if (!isSignalsReady) {
+    return <div className='init'>Initializing App...</div>
+  }
+
+  return <App />
+}
+
+export default Init;
